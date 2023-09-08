12e14ac80e112bbea2b2942db311c155b54c7b9f
Gordon Watts <gwatts@uw.edu>
2023-09-07 18:22:07 -0700

    Clean up for "nicer" plot infrastructure

--- cal_ratio_trainer/reporting/training_file.py
+++ cal_ratio_trainer/reporting/training_file.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Dict, Generator, List, Optional, Tuple, Union
+from typing import Callable, Generator, List, Optional, Tuple, Union
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -40,60 +40,6 @@ def find_column(name: List[str], file: Union[file_info, pd.DataFrame]) -> str:
         if n in d.columns:
             return n
     raise ValueError(f"Asked to find column {name} in file but it is not there.")
-
-
-def make_file_comparison_plot(files: List[file_info], col_names: Union[str, List[str]]):
-    # If the column names is a string, then make it a list.
-    if isinstance(col_names, str):
-        col_names = [col_names]
-
-    # Extract the data we will be plotting in common:
-    data = [
-        {"name": f.legend_name, "data": f.data[find_column(col_names, f)]}
-        for f in files
-    ]
-
-    # Now, make a histogram of the data using matplotlib.
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(1, 1, 1)
-    for d in data:
-        ax.hist(d["data"], bins=100, histtype="step", label=d["name"])
-
-    ax.legend()
-    ax.set_xlabel(col_names[0])
-    ax.set_ylabel("Number of Jets")
-    ax.set_title(f"{col_names[0]} by file")
-
-    return fig
-
-
-def make_label_comparison_plot(
-    file: file_info, col_names: Union[str, List[str]], label_names: Dict[int, str]
-):
-    # If the column names is a string, then make it a list.
-    if isinstance(col_names, str):
-        col_names = [col_names]
-
-    # Add a histogram for each label type:
-    col_name = find_column(col_names, file)
-
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(1, 1, 1)
-
-    for label in label_names.keys():
-        ax.hist(
-            file.data[file.data["label"] == label][col_name],
-            bins=100,
-            histtype="step",
-            label=f"{label_names[label]}",
-        )
-
-    ax.legend()
-    ax.set_xlabel(col_name)
-    ax.set_ylabel("Number of Jets")
-    ax.set_title(f"{col_name} By Data Type")
-
-    return fig
 
 
 def make_comparison_plots(
@@ -176,12 +122,13 @@ def make_report_plots(
         )
 
         # Plot the "headline" plots that are hopefully "in common".
-        if config.common_plots is not None:
-            report.header("## Common Plots")
-            for col_names in config.common_plots:
-                p = make_file_comparison_plot(files, col_names)
-                report.add_figure(p)
-                plt.close(p)
+        for p in plot_comparison_for_plot_list(
+            config.common_plots,
+            lambda: ((f.data, f.legend_name) for f in files),
+            "File",
+        ):
+            report.add_figure(p)
+            plt.close(p)
 
         # Next a list of all the columns:
         report.header("Some file specific information:")
@@ -190,19 +137,23 @@ def make_report_plots(
             report.header(f"### {f.legend_name} ({file_type} file)")
 
             # Repeat the common plots for the different data labels
-            if config.common_plots is not None:
-                labels = (
-                    config.data_labels_control
-                    if not f.is_signal
-                    else config.data_labels_signal
-                )
-                assert labels is not None
+            labels = (
+                config.data_labels_control
+                if not f.is_signal
+                else config.data_labels_signal
+            )
+            assert labels is not None
 
-                for col_names in config.common_plots:
-                    p = make_label_comparison_plot(f, col_names, labels)
-                    report.add_figure(p)
-                    plt.close(p)
-                report.write("")
+            for p in plot_comparison_for_plot_list(
+                config.common_plots,
+                lambda: (
+                    (f.data[f.data["label"] == k], v)
+                    for k, v in labels.items()  # type: ignore
+                ),
+                "Data Type",
+            ):
+                report.add_figure(p)
+                plt.close(p)
 
             # Total up the number of events for each mass category (llp_mH, llp_mS)
             # that is found in the file.
