87b8a4421d1be9e71795339fcae2556e45c6481c
Gordon Watts <gwatts@uw.edu>
2023-09-20 18:06:00 -0700

    Save the model on the way out

--- cal_ratio_trainer/convert/convert_json.py
+++ cal_ratio_trainer/convert/convert_json.py
@@ -1,5 +1,23 @@
+from pathlib import Path
 from cal_ratio_trainer.config import ConvertConfig
+from cal_ratio_trainer.reporting.evaluation_utils import load_trained_model
+from cal_ratio_trainer.utils import find_training_result
 
 
 def convert_file(c: ConvertConfig):
-    print(c)
+    # Load up the model and weights for the run.
+    assert c.run_to_convert is not None
+    model_path = find_training_result(c.run_to_convert.name, c.run_to_convert.run)
+    model = load_trained_model(model_path, c.run_to_convert.epoch)
+
+    # Now write it out!
+    assert c.output_json is not None
+    output_path = Path(c.output_json)
+
+    # Make sure the directory exists.
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Write out as keras output
+    model.model.save(output_path.with_suffix(".keras"))
+
+    # Next, convert it to fdeep format.
