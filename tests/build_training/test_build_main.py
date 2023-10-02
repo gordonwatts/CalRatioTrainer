5726f49b87a7138aef902b46bbaeaa7f8ba7897f
Gordon Watts <gwatts@uw.edu>
2023-10-01 22:58:39 -0700

    Test to check for proper column names

--- tests/build_training/test_build_main.py
+++ tests/build_training/test_build_main.py
@@ -27,6 +27,28 @@ def test_build_main_one_file(tmp_path, caplog):
     assert caplog.text == ""
 
 
+def test_build_sig_has_llp_columns(tmp_path, caplog):
+    out_file = tmp_path / "test_output.pkl"
+    c = BuildMainTrainingConfig(
+        input_files=[
+            training_input_file(
+                input_file=Path("tests/data/sig_311424_600_275.pkl"), num_events=None
+            )
+        ],
+        output_file=out_file,
+        min_jet_pT=30,
+        max_jet_pT=400,
+    )
+
+    build_main_training(c)
+
+    assert out_file.exists()
+    df = pd.read_pickle(out_file)
+
+    assert 'llp_mH' in df.columns
+    assert 'llp_mS' in df.columns
+
+
 def test_build_zero_length_file(tmp_path, caplog):
     out_file = tmp_path / "test_output.pkl"
     c = BuildMainTrainingConfig(
