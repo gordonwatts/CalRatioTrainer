5ead71c51602b60fe786a9904b64b684c4a53f62
Gordon Watts <gwatts@uw.edu>
2023-10-02 21:58:15 -0700

    Allow multiple copies of the `convert divertanalysis` to run simultaneously Fixes #120

---
+++ cal_ratio_trainer/common/file_lock.py
@@ -0,0 +1,29 @@
+import fcntl
+from pathlib import Path
+
+
+class FileLock:
+    def __init__(self, file_path: Path):
+        self.file_path = file_path.with_suffix(f"{file_path.suffix}.lock")
+        self.file = None
+
+    @property
+    def is_locked(self):
+        return self.file is not None
+
+    def __enter__(self):
+        "Lock the lock-file"
+        self.file = open(self.file_path, "w")
+        try:
+            fcntl.flock(self.file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except OSError:
+            self.file.close()
+            self.file = None
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file is not None:
+            fcntl.flock(self.file, fcntl.LOCK_UN)
+            self.file.close()
+            self.file = None
---
+++ tests/common/test_file_lock.py
@@ -0,0 +1,14 @@
+from cal_ratio_trainer.common.file_lock import FileLock
+
+
+def test_file_lock_simple(tmp_path):
+    with FileLock(tmp_path / "test_file.txt") as lock:
+        assert lock.is_locked
+
+
+def test_two_try(tmp_path):
+    interesting_file = tmp_path / "test_file.txt"
+    with FileLock(interesting_file) as lock1:
+        assert lock1.is_locked
+        with FileLock(interesting_file) as lock2:
+            assert not lock2.is_locked
